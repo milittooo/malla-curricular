75823ed023fcbc499ce8f05921b42962ee669509
milittooo <guerreromariamilagros3107@gmail.com>
2025-07-13 23:41:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,79 @@
 const malla = {
   "1er Año": {
-const materias = {
-  "Arquitectura de ordenadores personales": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: []
-      "Comunicación I": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: []
-      "Filosofía": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: []
-      "Pedagogía": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: []
-      "Práctica I": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: []
-      "Sistemas Operativos": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: []
-        "Taller de Aprendizaje Basado en Problemas": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: []
-        "Taller de Oralidad, Lectura y Escritura": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: []
-        "Teología I": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: []
-          "Software Ofimático I": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: []  
-        }
+    "Arquitectura de ordenadores personales": {
+      estado: "habilitada",
+      correlativas: []
+    },
+    "Comunicación I": {
+      estado: "habilitada",
+      correlativas: []
+    },
+    "Filosofía": {
+      estado: "habilitada",
+      correlativas: []
+    },
+    "Pedagogía": {
+      estado: "habilitada",
+      correlativas: []
+    },
+    "Práctica I": {
+      estado: "habilitada",
+      correlativas: []
+    },
+    "Sistemas Operativos": {
+      estado: "habilitada",
+      correlativas: []
+    },
+    "Taller de Aprendizaje Basado en Problemas": {
+      estado: "habilitada",
+      correlativas: []
+    },
+    "Taller de Oralidad, Lectura y Escritura": {
+      estado: "habilitada",
+      correlativas: []
+    },
+    "Teología I": {
+      estado: "habilitada",
+      correlativas: []
+    },
+    "Software Ofimático I": {
+      estado: "habilitada",
+      correlativas: []
+    }
   },
+
   "2do Año": {
-       "Comunicación II": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: ["Sistemas Operativos", "Comunicación I" ]  
-        "Didáctica General": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: ["Pedagogía"] 
-        "Teología II": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: [] 
-        "Lógica y Programación": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: ["Sistemas Operativos"] 
-          "Práctica II": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: ["Práctica I"] 
-          "Sujetos del Aprendizaje": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: ["Pedagogía"] 
-          "Tecnologías de la Información y la Comunicación I": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: [] 
-          "Software Ofimático II": {
-    estado: "habilitada", // "bloqueada", "habilitada", "aprobada"
-    correlativas: [Software Ofimático I] 
-    correlativas: ["Práctica I"] 
+    "Comunicación II": {
+      estado: "bloqueada",
+      correlativas: ["Sistemas Operativos", "Comunicación I"]
+    },
+    "Didáctica General": {
+      estado: "bloqueada",
+      correlativas: ["Pedagogía"]
+    },
+    "Teología II": {
+      estado: "bloqueada",
+      correlativas: []
+    },
+    "Lógica y Programación": {
+      estado: "bloqueada",
+      correlativas: ["Sistemas Operativos"]
+    },
+    "Práctica II": {
+      estado: "bloqueada",
+      correlativas: ["Práctica I"]
+    },
+    "Sujetos del Aprendizaje": {
+      estado: "bloqueada",
+      correlativas: ["Pedagogía"]
+    },
+    "Tecnologías de la Información y la Comunicación I": {
+      estado: "bloqueada",
+      correlativas: []
+    },
+    "Software Ofimático II": {
+      estado: "bloqueada",
+      correlativas: ["Software Ofimático I"]
+    }
+  }
+};
